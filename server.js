6c0c14ca5d5a441848db18f67cfddab064a5b55f
Glitch (ginger-aspiring-larkspur) <none>
2022-01-15 20:23:18 +0000

    🚌🎡 Checkpoint ./server.js:38782169/152

--- server.js
+++ server.js
@@ -53,3 +53,5 @@ server.listen(process.env.PORT || config.port, () => console.log(`${config.ssl ?
 
 
 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3601321214629909"
+     crossorigin="anonymous"></script>
